d7a8d8e98ae7a5cfe40a91543ae82c2906d238a4
Shakhawat Hossain <shakhawat-shihab>
2021-10-20 04:38:32 +0600

    banner edi

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,17 +4,17 @@ import map from '../../../src/map.PNG'
 const Footer = () => {
     return (
         <div >
-            <div className='row m-0 pt-3 pb-4 px-2 px-sm-3 justify-content-between align-items-center bg-footer'>
+            <div className='row g-5 m-0 pt-3 pb-4 px-2 px-sm-3 justify-content-around align-items-center bg-footer'>
                 <div className='col-md-4 col-sm-4 text-center'>
-                    <h3>Location</h3>
+                    <h4 className='pb-2'>Location</h4>
                     <div >
-                        <img src={map} alt="Google Map" width='80%' />
+                        <img src={map} alt="Google Map" width='70%' />
                     </div>
 
                 </div>
                 <div className='col-md-5 col-sm-5' id='contacts'>
                     <h4>Address</h4>
-                    <div className='d-flex flex-column pt-3'>
+                    <div className='d-flex flex-column '>
                         <h5 className='my-2 fw-bold'>Hospice Center</h5>
                         <p className='mb-2'> <span className='fw-bold'>Address:</span>  Plot-7/2, Section-2, Mirpur, Dhaka-1216, Bangladesh</p>
                         <p className='mb-2'> <span className='fw-bold'>Phone Number:</span> 58054708-12</p>
